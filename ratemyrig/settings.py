a83e16cf180deb05db04148cddd88029413efe6f
Damian Rodbari <skullphish@gmail.com>
2018-12-23 19:21:17 +0000

    Added the fix for django.contrib.messages

--- ratemyrig/settings.py
+++ ratemyrig/settings.py
@@ -116,6 +116,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+# to fix messages module 
+MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
