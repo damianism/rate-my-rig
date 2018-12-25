f2ea4a5f1b9e96d454efa6e59909f6a90a9f61a5
Damian Rodbari <skullphish@gmail.com>
2018-12-25 17:11:32 +0000

    Added a class based view - for testing purposes only!

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,23 @@
 from django.urls import path
 from . import views
+from .views import PostListView
 
 urlpatterns = [
-    path('',views.home, name='blog-home'),
+    # path('',views.home, name='blog-home'),
+    path('', PostListView.as_view() , name='blog-home'),
     path('about/',views.about, name='blog-about')
     ]
+    
+    
+"""
+class based views need the method ".as_view()" at the end to convert them
+into actual views
+
+    general convention for the list_template of class views
+    
+    <app>/<model>_<viewtype>.html - blog/posts_list.html 
+    
+    in this case:   
+        blog/posts_list.html    - blog being the app, posts being the model, and list being the viewtype
+    
+"""
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from posts.models import Post
+from django.views.generic import ListView
 
 
-# Create your views here.
+
+# function based views!
 def home(request):
     """ a view to render blog's home page """
     context = {
@@ -11,11 +13,25 @@ def home(request):
     
     return render(request, "blog/home.html", context)
     
-    
-    
+
 def about(request):
     """ view to render blog's about page  """
     context = {
         
     }
     return render(request, "blog/about.html", context)
+    
+
+# class based views
+
+class PostListView(ListView):
+    """ class base view to display all the posts """
+    
+    # where it looks for the template and what template
+    # <app>/<model>_<viewtype>.html     - e.g. blog/posts_list.html
+    
+    model = Post    # based on what class
+    
+    template_name = 'blog/home.html'    # to replace the default template (blog/posts_list.html) use "template_name"
+    # context_object_name = 'posts'     # to replace the default context name (object_list) use "context_object_name"
+    ordering = ['-date_posted']         # change the order  - ['-date_posted'] to reverse the order
