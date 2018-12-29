1b8043b47dd372518d8d8cea0ed9413eb0faafa8
Damian Rodbari <skullphish@gmail.com>
2018-12-29 14:52:03 +0000

    changed method to serve media, modified post_form.html

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+# from django.views.generic import RedirectView
+
 from .views import (
     # PostListView, 
     PostCreateView,
--- ratemyrig/urls.py
+++ ratemyrig/urls.py
@@ -14,18 +14,22 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 # handle media files in developement - from django documentation
 from django.conf import settings
 from django.conf.urls.static import static
+# new 
+from django.views.static import serve
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('user/', include('users.urls')),
+    path('media/', include('users.urls')),
+    re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
 ]
 
-# this approach only works locally, WONT work on deployment
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# # this approach only works locally, WONT work on deployment
+# if settings.DEBUG:
+#     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
