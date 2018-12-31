609e190780c88d7f368ea00ca0cbd7d9ab53237d
Damian Rodbari <skullphish@gmail.com>
2018-12-31 20:58:32 +0000

    Added some more styling and resized textarea widget in comments forms

--- comments/forms.py
+++ comments/forms.py
@@ -2,7 +2,17 @@ from django import forms
 from .models import Comment
 
 class CommentForm(forms.ModelForm):
-
+    
+    comment = forms.CharField(
+                    widget=forms.Textarea(
+                        attrs={
+                            "placeholder": "Your comment",
+                            "rows": 3,
+                            "cols": 50
+                            }
+                        )
+                    )
+    
     class Meta:
         model = Comment
         fields = ('comment', 'post','author',)
