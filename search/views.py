5855e17d39eb09f57ad5f0e2f7e4b1bc4b3d6811
Damian Rodbari <skullphish@gmail.com>
2019-01-03 13:02:09 +0000

    added WORKING pagination to search results

--- search/views.py
+++ search/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from blog.models import Post
-
+from django.core.paginator import Paginator
 
 def do_search(request):
     
+    type_session = request.session.get('type',None)
+    print("type_session = ", type_session)
+    
     selections = ["title", "gpu", "cpu", "ram", "psu", "mainboard" ]
     search_type = request.GET.get('type')
-
+    if search_type is None:
+        search_type = type_session
+    
+    
+    print(">>>>>>>>>>>>>>>>>search_type", search_type)
     # DOING A FOR LOOP HERE DIDNT WORK! IT DOESNT SEEM TO LIKE IT!
     if search_type == "title":   posts = Post.objects.filter(title__icontains=request.GET['q'])
     elif search_type == "gpu":   posts = Post.objects.filter(gpu__icontains=request.GET['q'])
@@ -15,13 +22,20 @@ def do_search(request):
     elif search_type == "psu":   posts = Post.objects.filter(psu__icontains=request.GET['q'])
     elif search_type == "mainboard":   posts = Post.objects.filter(mainboard__icontains=request.GET['q'])
     
+    paginator = Paginator(posts, 5) # Show 5 posts per page
+    page = request.GET.get('page')
+    posts = paginator.get_page(page)
+
     context = {
         "object_list": posts,
         "selections": selections,
         "type": search_type
     }
     
+    request.session['type'] = search_type
     return render(request, "search/search.html", context )
+    
+    
     
 def pre_search(request):
 
