95e43dc05fe97c4126457b7cbf8513a7cceedd67
Damian Rodbari <skullphish@gmail.com>
2019-01-04 23:00:29 +0000

    removed the function. imported items instead

--- blog/helper_funcs.py
+++ blog/helper_funcs.py
@@ -1,69 +1,58 @@
 
-def get_choices():
-    """ Set default choices for the Post model fields """
-    
-    
-    # BUILD_CHOICES = (
-    #     ('AMD', 'AMD'),
-    #     ('INTEL', 'INTEL'),
-    # )
-    
-    RAM_CHOICES = (
-        ('4GB', '4GB'),
-        ('8GB', '8GB'),
-        ('16GB', '16GB'),
-        ('32GB', '32GB'),
-        ('64GB', '64GB'),
-        ('128GB', '128GB'),
+RAM_CHOICES = (
+    ('4GB', '4GB'),
+    ('8GB', '8GB'),
+    ('16GB', '16GB'),
+    ('32GB', '32GB'),
+    ('64GB', '64GB'),
+    ('128GB', '128GB'),
 
-    )
-        
-    PSU_CHOICES = (
-        ('400W', '400W'),
-        ('450W', '450W'),
-        ('500W', '500W'),
-        ('550W', '550W'),
-        ('600W', '600W'),
-        ('650W', '650W'),
-        ('700W', '700W'),
-        ('750W', '750W'),
-        ('800W', '800W'),
-        ('850W', '850W'),
-        ('1000W', '1000W'),
+)
+    
+PSU_CHOICES = (
+    ('400W', '400W'),
+    ('450W', '450W'),
+    ('500W', '500W'),
+    ('550W', '550W'),
+    ('600W', '600W'),
+    ('650W', '650W'),
+    ('700W', '700W'),
+    ('750W', '750W'),
+    ('800W', '800W'),
+    ('850W', '850W'),
+    ('1000W', '1000W'),
 
-    )
-    
-    CPU_CHOICES = (
-        ('i3 Gen-7', 'i3 Gen-7'),
-        ('i3 Gen-8', 'i3 Gen-8'),
-        ('i5 Gen-7', 'i5 Gen-7'),
-        ('i5 Gen-8', 'i5 Gen-8'),
-        ('i7 Gen-7', 'i7 Gen-7'),
-        ('i7 Gen-8', 'i7 Gen-8'),
-        ('i9 Gen-7', 'i9 Gen-7'),
-        ('i9 Gen-8', 'i9 Gen-8'),
-        ('Ryzen 3 Gen-1', 'Ryzen 3 Gen-1'),
-        ('Ryzen 3 Gen-2', 'Ryzen 3 Gen-2'),
-        ('Ryzen 5 Gen-1', 'Ryzen 5 Gen-1'),
-        ('Ryzen 5 Gen-2', 'Ryzen 5 Gen-2'),
-        ('Ryzen 7 Gen-1', 'Ryzen 7 Gen-1'),
-        ('Ryzen 7 Gen-2', 'Ryzen 7 Gen-2'),
-        ('Ryzen Threadripper Gen-1', 'Ryzen Threadripper Gen-1'),
-        ('Ryzen Threadripper Gen-2', 'Ryzen Threadripper Gen-2'),
-    )  
-    
-    GPU_CHOICES = (
-        ('GTX 1060', 'GTX 1060'),
-        ('GTX 1060Ti', 'GTX 1060Ti'),
-        ('GTX 1070', 'GTX 1070'),
-        ('GTX 1070Ti', 'GTX 1070Ti'),
-        ('GTX 1080', 'GTX 1080'),
-        ('GTX 1080Ti', 'GTX 1080Ti'),
-        ('GTX 2070', 'GTX 2070'),
-        ('GTX 2070ti', 'GTX 2070Ti'),
-        ('GTX 2080', 'GTX 2080'),
-        ('GTX 2080Ti', 'GTX 2080Ti'),
+)
 
-    )
-    
-    return RAM_CHOICES, PSU_CHOICES, CPU_CHOICES, GPU_CHOICES
+CPU_CHOICES = (
+    ('i3 Gen-7', 'i3 Gen-7'),
+    ('i3 Gen-8', 'i3 Gen-8'),
+    ('i5 Gen-7', 'i5 Gen-7'),
+    ('i5 Gen-8', 'i5 Gen-8'),
+    ('i7 Gen-7', 'i7 Gen-7'),
+    ('i7 Gen-8', 'i7 Gen-8'),
+    ('i9 Gen-7', 'i9 Gen-7'),
+    ('i9 Gen-8', 'i9 Gen-8'),
+    ('Ryzen 3 Gen-1', 'Ryzen 3 Gen-1'),
+    ('Ryzen 3 Gen-2', 'Ryzen 3 Gen-2'),
+    ('Ryzen 5 Gen-1', 'Ryzen 5 Gen-1'),
+    ('Ryzen 5 Gen-2', 'Ryzen 5 Gen-2'),
+    ('Ryzen 7 Gen-1', 'Ryzen 7 Gen-1'),
+    ('Ryzen 7 Gen-2', 'Ryzen 7 Gen-2'),
+    ('Ryzen Threadripper Gen-1', 'Ryzen Threadripper Gen-1'),
+    ('Ryzen Threadripper Gen-2', 'Ryzen Threadripper Gen-2'),
+)  
+
+GPU_CHOICES = (
+    ('GTX 1060', 'GTX 1060'),
+    ('GTX 1060Ti', 'GTX 1060Ti'),
+    ('GTX 1070', 'GTX 1070'),
+    ('GTX 1070Ti', 'GTX 1070Ti'),
+    ('GTX 1080', 'GTX 1080'),
+    ('GTX 1080Ti', 'GTX 1080Ti'),
+    ('GTX 2070', 'GTX 2070'),
+    ('GTX 2070ti', 'GTX 2070Ti'),
+    ('GTX 2080', 'GTX 2080'),
+    ('GTX 2080Ti', 'GTX 2080Ti'),
+
+)
--- blog/models.py
+++ blog/models.py
@@ -2,23 +2,19 @@ from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
 from django.urls import reverse
-from .helper_funcs import *
+from .helper_funcs import RAM_CHOICES, PSU_CHOICES, CPU_CHOICES, GPU_CHOICES
 from decimal import Decimal
 # third party package to resize images
 from django_resized import ResizedImageField
 
 class Post(models.Model):
-    """ Create model for a typical post """
+    """ Create model for a typical build """
     
-    title             = models.CharField(max_length=60)
-    
-    # get choices
-    RAM_CHOICES, PSU_CHOICES, CPU_CHOICES, GPU_CHOICES = get_choices()
-    
-    cpu = models.CharField(max_length=30, choices=CPU_CHOICES, default='i5 Gen-8')
-    gpu = models.CharField(max_length=30, choices=GPU_CHOICES, default='GTX 1080')
-    ram = models.CharField(max_length=30, choices=RAM_CHOICES, default='8GB')
-    psu = models.CharField(max_length=30, choices=PSU_CHOICES,default='750W')
+    title = models.CharField(max_length=60)
+    cpu   = models.CharField(max_length=30, choices=CPU_CHOICES, default='i5 Gen-8')
+    gpu   = models.CharField(max_length=30, choices=GPU_CHOICES, default='GTX 1080')
+    ram   = models.CharField(max_length=30, choices=RAM_CHOICES, default='8GB')
+    psu   = models.CharField(max_length=30, choices=PSU_CHOICES,default='750W')
 
     primary_storage   = models.CharField(max_length=120)
     secondary_storage = models.CharField(max_length=120, blank=True)
@@ -27,16 +23,14 @@ class Post(models.Model):
     date_posted       = models.DateTimeField(default=timezone.now)
     views             = models.IntegerField(default=0)
     image             = ResizedImageField(default='defaults/default_rig_pic.png', upload_to='posts_pics', size=[500, 500], blank=True)
-    price = models.DecimalField(max_digits=5, decimal_places=2, default=Decimal(100.00))
+    price             = models.DecimalField(max_digits=5, decimal_places=2, default=Decimal(100.00))
     author            = models.ForeignKey(User, on_delete=models.CASCADE)     #  if the user was deleted - delete all his/her post
                                                                               #  if the post of deleted - user will remain intact
-
     # difference between redirect() and reverse()
     # redirect:  redirects you to a specific route
     # reverse:   returns the full url as string to a specific route 
     def get_absolute_url(self):
         return reverse("post-detail", kwargs={"pk": self.pk}) 
-    
     
     def __str__(self):
         return self.title
