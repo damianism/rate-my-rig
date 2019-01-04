cfe008b15c1005f5558731fed7867dcd8f4871f6
Damian Rodbari <skullphish@gmail.com>
2019-01-04 23:16:19 +0000

    cart access grand to users not authenticated

--- cart/views.py
+++ cart/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render, redirect, reverse
-from django.contrib.auth.decorators import login_required
 from django.contrib import messages 
 from django.http import HttpResponseRedirect
 
-@login_required
+
 def view_cart(request):
     """
     View to render contents of the cart
@@ -13,21 +12,15 @@ def view_cart(request):
     return render(request, "cart/cart.html")
 
 
-@login_required
+
 def add_to_cart(request, pk):
     """ 
     Add a quantity of the specified product to the cart
     """
-    # pk = str(pk)
+
     # get quantity out of the form
-    try:
-        quantity = int( request.POST.get("quantity") )
-    except TypeError:
-        # preventing TypeError on cloud9 and Server Error (500) on heroku
-        messages.info(request, "You may now add to cart.")
-        return redirect( 'blog-home' )
-        # DOESNT WORK!! return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
-        
+    quantity = int( request.POST.get("quantity") )
+    
     # get cart out of session if exists
     cart = request.session.get("cart", {} )
 
@@ -49,16 +42,11 @@ def add_to_cart(request, pk):
     #   - if item added from post_detail template, redirect back to post_detail template
     #   - if item added from home template, redirect back to home template
     previous_loc = request.META.get('HTTP_REFERER')
-    print("previous_loc = ", previous_loc)
+
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', '/'))
-    # NOTE! ASK ABOUT THIS! REMOVE IF US OF "HttpResponseRedirect" IS OK!
-    # if "post" in previous_loc:
-    #     return redirect( 'post-detail', pk=pk )
-    # else:
-    #     return redirect( 'blog-home' )
+
     
-    
-@login_required  
+
 def adjust_cart(request, pk):
     """
     view to adjust the quantity of the specified post to the specified ammount 
