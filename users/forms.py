14963134ad75d48204fcb18e3adbd29bd485dd4d
Damian Rodbari <skullphish@gmail.com>
2019-01-05 18:46:24 +0000

    fixed a bug with email not being return after cleaning

--- users/forms.py
+++ users/forms.py
@@ -9,7 +9,7 @@ class UserRegisterForm(UserCreationForm):
     """    
 
     # Fields to add/edit
-    email = forms.EmailField()
+    email = forms.EmailField(required=True)
     first_name = forms.CharField(max_length=15)
     last_name = forms.CharField(max_length=30)
     class Meta:
@@ -34,7 +34,7 @@ class UserRegisterForm(UserCreationForm):
         email = self.cleaned_data.get("email")
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("A user with that email address already exists.")
-
+        return email
             
 class UserUpdateFrom(forms.ModelForm):
     """ Form to change username and email from user profile template """
