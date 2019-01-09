1c9a0bc467a7873d680306ef51d17b035ed0f97d
Damian Rodbari <skullphish@gmail.com>
2019-01-09 20:41:28 +0000

    cleaned up blog.urls

--- blog/urls.py
+++ blog/urls.py
@@ -13,19 +13,4 @@ urlpatterns = [
     path('post/<int:pk>/update', PostUpdateView.as_view() , name='post-update'),
     path('post/<int:pk>/delete', PostDeleteView.as_view() , name='post-delete'),
     path('post/new/', PostCreateView.as_view() , name='post-create'),
-    # path('about/',views.about, name='blog-about')
     ]
-    
-    
-"""
-class based views need the method ".as_view()" at the end to convert them
-into actual views
-
-    general convention for the list_template of class views
-    
-    <app>/<model>_<viewtype>.html - blog/posts_list.html 
-    
-    in this case:   
-        blog/posts_list.html    - blog being the app, posts being the model, and list being the viewtype
-    
-"""
