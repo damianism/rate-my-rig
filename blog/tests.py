68962560494457151e2084684c449b3bc214ddf0
Damian Rodbari <skullphish@gmail.com>
2019-01-11 21:14:14 +0000

    switched to sqlite to test the blog app

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+from .forms import BlogPostForm
 
-# Create your tests here.
+class TestBlogPostForm(TestCase):
+    
+    def test_can_create_a_post_with_a_name(self):
+        form = BlogPostForm(
+            {
+                "title":"test system",
+                "cpu":"i5 Gen-8",
+                "gpu":"GTX 1080",
+                "ram":"8GB",
+                "psu":"750W",
+                "mainboard": "ASUS TEST",
+                "primary_storage": "SSD TEST"
+            }
+        )
+        self.assertTrue(form.is_valid())
+        
+        
+    def test_correct_message_for_missing_required_field(self):
+        form = BlogPostForm(
+            {
+                "title":"test system",
+                "cpu":"i5 Gen-8",
+                "gpu":"GTX 1080",
+                "ram":"",
+                "psu":"750W",
+                "mainboard": "ASUS TEST",
+                "primary_storage": "SSD TEST"
+            }
+        )
+        self.assertFalse(form.is_valid())
+        self.assertEqual(form.errors['ram'], [u'This field is required.'])
+        
